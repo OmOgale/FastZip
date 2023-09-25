a800042666c5aa6a04e3b3ac550e42e41fc6632a
OmOgale <123871192+OmOgale@users.noreply.github.com>
2023-09-25 03:37:51 -0400

    Updating decoder

--- decoder.py
+++ decoder.py
@@ -1,12 +1,28 @@
+with open("compressed.bin", "rb") as fh:
+    bin_map_data = fh.readline()
+    bin_data = fh.read()
+
+map_str = bin_map_data.decode("utf-8")
+sym_lst = map_str.split(";")
+sym_map = {}
+
+for a in sym_lst:
+    if a[2::] in sym_map:
+        continue
+    sym_map[a[2::]] == a[0]
+
+bin_str = bin_data.decode("utf-8")
 search_str = ""
 make_str = ""
-n = len(make)
-for a in range(n):
-    search_str += make[a]
-    if search_str in map:
-        make_str += map[search_str]
+n = len(bin_data)
+
+for m in range(n):
+    search_str += bin_data[m]
+    if search_str in sym_map:
+        make_str += sym_map[search_str]
         search_str = ""
 
-with open("output.txt", "w") as fh:
-    fh.write(make_str)
+with open("uncompressed.txt", "w+") as fw:
+    fw.write(make_str)
+
 
